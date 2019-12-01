4c1b0a927a6975c6c76d9148de480f68f8d1d159
Nicholas George <nicholas.george32@gmail.com>
2019-12-01 10:57:59 -0700

    updated publish.el and deleted old content

--- publish.el
+++ publish.el
@@ -2,4 +2,25 @@
 ;; based on: https://stackoverflow.com/questions/46295511/how-to-run-org-mode-commands-from-shell
 (add-to-list 'load-path "~/.emacs.d/elpy")
 (require 'org)
+
 (load-theme leuveun)
+
+(setq org-publish-project-alist
+      '(
+        ("programming"
+         :base-directory "~/personal_projects/website-clj/resources/org-programming"
+         :base-extension "org"
+         :publishing-directory "~/personal_projects/website-clj/resources/programming"
+         :publishing-function org-html-publish-to-html
+         :headline-levels 4
+         :html-extension "html"
+         :body-only t)
+        ("science"
+         :base-directory "~/personal_projects/website-clj/resources/org-science"
+         :base-extension "org"
+         :publishing-directory "~/personal_projects/website-clj/resources/science"
+         :publishing-function org-html-publish-to-html
+         :headline-levels 4
+         :html-extension "html"
+         :body-only t)
+        ("clj-site" :components ("programming" "science"))))
