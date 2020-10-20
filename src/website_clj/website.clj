47052e272ae68d1e0e09ec894232f89fb829f45c
Nicholas George <nicholas.george32@gmail.com>
2020-10-19 22:22:37 -0600

    simplify, get rid of optimus

--- src/website_clj/website.clj
+++ src/website_clj/website.clj
@@ -1,11 +1,6 @@
 (ns website-clj.website
   "main namespace for building and exporting the website"
-  (:require [optimus.assets :as assets]
-            [optimus.export]
-            [optimus.optimizations :as optimizations]      
-            [optimus.prime :as optimus]                    
-            [optimus.strategies :refer [serve-live-assets]]
-            [clojure.string :as str]
+  (:require [clojure.string :as str]
             [stasis.core :as stasis]
             [website-clj.export-helpers :as helpers]
             [website-clj.process-pages :as process]))
@@ -43,11 +38,6 @@
   (apply str (for [x (keys (merge prog-metadata sci-metadata))]
                (str "http://nickgeorge.net" x "/\n" "https://nickgeorge.net" x "/\n"))))
 
-(defn get-assets
-  "get all static assets from the public directory."
-  []
-  (assets/load-assets "public" [#".*"]))
-
 (defn get-pages
   "gets all pages and assets for testing and deployment"
   []
@@ -58,26 +48,21 @@
         homepage (home-page-header)
         all-links (process/merge-maps-sort-take-five prog-meta sci-meta)]
     (stasis/merge-page-sources
-     {:public (stasis/slurp-directory  "resources/public" #".*\.(html|css|js)$")
+     {:public (stasis/slurp-directory  "resources/public" #".*\.(html|css|js|png|jpg)$")
       :landing (process/add-links-to-map all-links :#recentPosts homepage) 
       :programming (process/add-links-to-map (process/format-html-links prog-meta) :#pageListDiv programming-map)
       :science (process/add-links-to-map (process/format-html-links sci-meta) :#pageListDiv science-map)
       :robots (hash-map "/robots.txt" "User-agent: *\nDisallow:\nSITEMAP: http://nickgeorge.net/sitemap.txt")
       :sitemap (hash-map "/sitemap.txt" (make-site-map prog-meta sci-meta))})))
 
-(def app
-  "renders the website for experimentation"
-  (optimus/wrap
-   (stasis/serve-pages get-pages)
-   get-assets
-   optimizations/none
-   serve-live-assets))
 
 ;; main export function, called by lein build-site
 (defn export
   "main export function for static site. See docs for functions included."
   []
-  (let [assets (optimizations/all (get-assets) {})]
-    (helpers/clear-directory! export-dir)
-    (optimus.export/save-assets assets export-dir)
-    (stasis/export-pages (get-pages) export-dir {:optimus-assets assets})))
+  (helpers/clear-directory! export-dir)
+  (stasis/export-pages (get-pages) export-dir))
+
+(def app
+  "preview app"
+  (stasis/serve-pages get-pages))
