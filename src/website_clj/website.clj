4fc53b6c1c463d1dae5692ae4384f0f3c1f064a9
Nicholas George <nicholas.george32@gmail.com>
2020-10-25 17:38:31 -0600

    cache bust css and add files in header to all pages.

--- src/website_clj/website.clj
+++ src/website_clj/website.clj
@@ -5,28 +5,20 @@
   "main namespace for building and exporting the website"
   (:require [clojure.string :as str]
             [stasis.core :as stasis]
+            [hiccup.core :refer [html]]
+            [hiccup.page :refer [html5 include-css include-js]]
+            [hiccup.element :refer [link-to]]
             [digest :as digest]
             [net.cgrand.enlive-html :as enlive]
             [clojure.edn :as edn]
-            [website-clj.export-helpers :as helpers]
-            [website-clj.process-pages :as process]))
+            [website-clj.export-helpers :as helpers]))
 
 (def export-dir "target/nickgeorge.net")
 
-(defn remove-temps [m]
-  (apply dissoc m (filter #(re-matches #".*\.html~" %) (keys m))))
 
-(defn make-page-map
-  "Reads all the html using `stasis/slurp-directory` and apply the html formatting
-  and path formatting."
-  [relative-path]
-  (let [base (second (str/split relative-path #"/"))
-        page-map (remove-temps (stasis/slurp-directory relative-path  #".*\.(html|css|js)"))
-        page-html (vals page-map)
-        page-keys (map #(str "/" base %) (map #(str/replace % #"(?<!index)\.html$" "") (keys page-map)))]
-    (->> page-html
-         (map process/format-html)
-         (zipmap page-keys))))
+(defn get-copyright-date []
+  (.format (java.text.SimpleDateFormat. "yyyy")
+           (new java.util.Date)))
 
 (defn cache-bust-css [path]
   "hash css file value, rename file with first 8 digist of digest. Return a map of css-renmaed-path, css-val"
@@ -38,52 +30,29 @@
         new-keys (map #(str/replace %1 ".css" (str "-" %2 ".css")) css-keys short-hash)]
     (zipmap new-keys css-vals)))
 
-(defn home-page-header
-  "Formatting for the landing/home page"
-  []
-  (let [hp-map (stasis/slurp-directory  "resources/home" #".*\.(html|css|js)$")
-        hp-map-keys (keys hp-map)
-        hp-html (vals hp-map)]
-    (->> hp-html
-         (map process/format-html)
-         (zipmap hp-map-keys))))
+(defn apply-header-footer
+  "Applies a header and footer and css to an html strings."
+  [css-seq page]
+  (html5 {:lang "en"}
+         [:head
+          #_[:script {:src "https://www.googletagmanager.com/gtag/js?id=UA-124749948-1" :async "async"}]
+          #_[:script google-analytics]
+          [:title "Nick's site"]
+          [:meta {:charset "utf-8"}]
+          [:meta {:name "viewport"
+                  :content "width=device-width, initial-scale=1.0"}]
+          (for [css css-seq] [:link {:type "text/css", :href css :rel "stylesheet"}])]
+         [:body
+          [:div {:class "header"}
+           [:div {:class "name"}
+            [:a {:class "name" :href "/"} "Nick George"]
+            [:div {:class "header-right"}
+             [:a {:href "/science"} "Science"]
+             [:a {:href "/programming"} "Programming"]]]]
+          page]
+         [:footer
+          [:p (str "&copy Nick George 2017-") (get-copyright-date)]]))
 
-(defn make-site-map
-  "compile a list of all the pages for search engines."
-  [sci-metadata prog-metadata]
-  (apply str (for [x (keys (merge prog-metadata sci-metadata))]
-               (str "http://nickgeorge.net" x "/\n" "https://nickgeorge.net" x "/\n"))))
-
-(defn get-pages
-  "gets all pages and assets for testing and deployment"
-  []
-  (let [programming-map (make-page-map "resources/programming")
-        prog-meta (process/make-edn-page-map programming-map)
-        science-map (make-page-map "resources/science")
-        sci-meta (process/make-edn-page-map science-map)
-        homepage (home-page-header)
-        all-links (process/merge-maps-sort-take-five prog-meta sci-meta)]
-    (stasis/merge-page-sources
-     {:public (stasis/slurp-directory  "resources/public" #".*\.(html|css|js|png|jpg)$")
-      :landing (process/add-links-to-map all-links :#recentPosts homepage) 
-      :programming (process/add-links-to-map (process/format-html-links prog-meta) :#pageListDiv programming-map)
-      :science (process/add-links-to-map (process/format-html-links sci-meta) :#pageListDiv science-map)
-      :robots (hash-map "/robots.txt" "User-agent: *\nDisallow:\nSITEMAP: http://nickgeorge.net/sitemap.txt")
-      :sitemap (hash-map "/sitemap.txt" (make-site-map prog-meta sci-meta))})))
-
-
-;; main export function, called by lein build-site
-(defn export
-  "main export function for static site. See docs for functions included."
-  []
-  (helpers/clear-directory! export-dir)
-  (stasis/export-pages (get-pages) export-dir))
-
-#_(def app
-  "preview app"
-  (stasis/serve-pages get-pages))
-
-;;;; Scratch/repl play ;;;;
 
 (defn parse-edn
   "returns edn metadata for page-text or nil"
@@ -94,22 +63,44 @@
 
 
 
-(defn get-pages-simple
+(defn get-pages
   "gets all pages and assets for website"
   []
   (let [all-pages-map (stasis/slurp-directory "resources/" #".*\.html$")
         all-pages-keys (keys all-pages-map)
-        all-pages-vals (map process/apply-header-footer (vals all-pages-map))
+        css-hashed (cache-bust-css "resources/public") ;; keys needed for later
+        css-keys (keys css-hashed)
+        header-footer-partial (partial apply-header-footer css-keys) ;; apply first arg for hashed css names
+        all-pages-vals (map header-footer-partial (vals all-pages-map)) ;; map over pages
         edn-all (map parse-edn all-pages-vals)]
-    (zipmap all-pages-keys all-pages-vals)
-    ))
+    
+    (stasis/merge-page-sources
+     {:pages
+      (zipmap all-pages-keys all-pages-vals)
+      :css css-hashed
+      :img (stasis/slurp-directory "resources/public" #".*\.(png|jpg)$")})))
 
 
-(get-pages-simple)
+(defn make-site-map
+  "compile a list of all the pages for search engines."
+  [sci-metadata prog-metadata]
+  (apply str (for [x (keys (merge prog-metadata sci-metadata))]
+               (str "http://nickgeorge.net" x "/\n" "https://nickgeorge.net" x "/\n"))))
+
+
+;; main export function, called by lein build-site
+(defn export
+  "main export function for static site. See docs for functions included."
+  []
+  (helpers/clear-directory! export-dir)
+  (stasis/export-pages (get-pages) export-dir))
+
+;;;; Scratch/repl play ;;;;
+
 
 (def app
   "preview app"
-  (stasis/serve-pages get-pages-simple))
+  (stasis/serve-pages get-pages))
 ;; TODO apply html formatting, title adding, etc.
 ;; TODO fix fns that add links to landing pages
 ;; In make-page-map you only need a :pages, :css, and :images
