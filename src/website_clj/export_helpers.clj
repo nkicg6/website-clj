3b32fce5e681c6f52b40354c647107b27a6846a4
Nicholas George <nicholas.george32@gmail.com>
2020-11-02 22:18:22 -0700

    added helper to clear target except for .git

--- src/website_clj/export_helpers.clj
+++ src/website_clj/export_helpers.clj
@@ -1,38 +1,18 @@
 (ns website-clj.export-helpers
   "helper functions for saving the git directory, cname, and gitignore from `stasis/empty-directory!`
   This exists to help with rendering static sites on github." 
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]
+            [me.raynes.fs :as fs]))
 
-(defn make-filtered-list
-  ;; this would be a good use for a macro. pass in the vector of exclusions, and generate an arbitrary len of
-  ;; exclude predicates.
-  [base]
-  (let [flist  (mapv str (filter #(.isFile %) (file-seq (clojure.java.io/file base))))]
-     (filter (apply every-pred [#(not (str/includes? % ".git")) #(not (str/includes? % "CNAME"))]) flist)))
+(defn- delete! [path]
+  (cond
+    (fs/directory? path) (fs/delete-dir path)
+    (fs/file? path) (fs/delete path)
+    :else "None"))
 
-(defn clear-directory!
-  [base]
-  (map clojure.java.io/delete-file (make-filtered-list  base)))
-
-;;(clear-directory! "/Users/nick/test_dir")
-(def test-dir "/Users/nick/Desktop/test")
-
-(defn- make-file-list
-  "returns a filtered list of file paths"
-  [dir]
-  (->> (file-seq (clojure.java.io/file dir))
-       (map str)
-       (filter #(not (str/includes? % "CNAME")))
-       (filter #(not (str/includes? % ".git")))
-       (filter #(not (= % dir)))))
-
-(defn clear-directory!
-  [str-files]
-  (let [as-files (map clojure.java.io/file str-files)
-        files (filter #(.isFile %) as-files)
-        dirs (filter #(.isDirectory %) as-files)]
-    (map clojure.java.io/delete-file files)
-    (map clojure.java.io/delete-file dirs)))
-
-(clear-directory! (make-file-list test-dir))
-
+(defn clear-directory! [target]
+  "Deletes all files and directories from target except .git."
+  (let [as-files (fs/list-dir target)
+        no-git (filter #(not (str/includes? % ".git")) as-files)]
+    (map delete! no-git)))
